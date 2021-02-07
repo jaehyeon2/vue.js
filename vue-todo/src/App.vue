94de677235089e9b9680c8d53325e6ea63d2e5cb
강재현 <wol5971@gmail.com>
2021-02-07 10:52:59 +0000

    TodoHeader add

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	import TodoHeader from './components/TodoHeader.vue';
+	import TodoInput from './components/TodoInput.vue';
+	import TodoList from './components/TodoList.vue';
+	import TodoFooter from './components/TodoFooter.vue';
+	
+	var TodoHeader={
+		template:"<div>header</div>"
+	};
+	export default {
+		components:{
+			"TodoHeader" : TodoHeader
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
