f1fed924245fb67c08fe99ecf5d17416d2b6faa6
강재현 <wol5971@gmail.com>
2021-02-07 11:06:15 +0000

    syntax error commit

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -7,15 +7,6 @@
 	import TodoInput from './components/TodoInput.vue';
 	import TodoList from './components/TodoList.vue';
 	import TodoFooter from './components/TodoFooter.vue';
-	
-	var TodoHeader={
-		template:"<div>header</div>"
-	};
-	export default {
-		components:{
-			"TodoHeader" : TodoHeader
-		}
-	}
 </script>
 
 <style>
