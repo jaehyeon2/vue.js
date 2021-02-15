f095acaf2aa14b6ac514d04d030117018666214a
강재현 <wol5971@gmail.com>
2021-02-15 10:29:33 +0000

    router test

--- vue-board/backend/routes/index.js
+++ vue-board/backend/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const api = require("./api"); //api router 호출
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("path="+req.path);
+  next();
 });
+
+router.use("/api",api); //api router 적용
+
+router.all('/',(req,res)=>{
+  res.send("{message:welcome}");
+})
 
 module.exports = router;
